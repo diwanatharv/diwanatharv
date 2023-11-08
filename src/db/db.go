b884609ec7b7fd06e38885c3c675b1fef09c2bdf
Muthu Durai <muthu@kloudone.com>
2023-11-08 16:41:19 +0530

    db fixes

--- src/db/db.go
+++ src/db/db.go
@@ -1,28 +1,46 @@
+
 package db
 
 import (
 	"fmt"
-	"log"
+	"sync"
 
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func Makegormserver() *gorm.DB {
+var once sync.Once
+var dba *gorm.DB
 
-	env := viper.GetString("env") + "."
-	user := viper.GetString(env + "db.user")
-	password := viper.GetString(env + "db.password")
-	host := viper.GetString(env + "db.host")
-	port := viper.GetString(env + "db.port")
-	dbname := viper.GetString(env + "db.name")
-	schema := viper.GetString(env + "db.schema")
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable search_path=%s", host, user, password, dbname, port, schema)
-	db, err := gorm.Open(postgres.Open(dsn))
-	if err != nil {
-		log.Print(err.Error())
-		log.Fatalln("error in setting up db connection")
-	}
-	return db
+func Makegormserver() *gorm.DB {
+	once.Do(func() {
+
+		env := viper.GetString("env") + "."
+		user := viper.GetString(env + "db.user")
+		password := viper.GetString(env + "db.password")
+
+		host := viper.GetString(env + "db.host")
+		port := viper.GetString(env + "db.port")
+		dbname := viper.GetString(env + "db.name")
+		schema := viper.GetString(env + "db.schema")
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require search_path=%s", host, user, password, dbname, port, schema)
+		db, err := gorm.Open(postgres.New(postgres.Config{
+			DSN:                  dsn,
+			PreferSimpleProtocol: true,
+		}), &gorm.Config{})
+		dba = db
+		if err != nil {
+			log.Panic().Msgf("Error connecting to the database at %s:%s/%s", host, port, dbname)
+		}
+		sqlDB, err := dba.DB()
+		if err != nil {
+			log.Panic().Msgf("Error getting GORM DB definition")
+		}
+		sqlDB.SetMaxIdleConns(10)
+		sqlDB.SetMaxOpenConns(100)
+		log.Info().Msgf("Successfully established connection to %s:%s/%s", host, port, dbname)
+	})
+	return dba
 }
